4620db24bcc08d64dd87f798b78c7fb4bb2f555d
Vivek kumar <vivekkumar52522@gmail.com>
2023-05-30 04:11:00 -0700

    Day_12

---
+++ Day_12/LexicographicalNum.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(int i,int n){
+    if(i>n) return;
+
+    cout<<i<<" ";
+    for(int j=0;j<10;j++){
+       solve(10*i+j,n);
+    }
+}
+int lexicographicalNumber(int n){
+    for(int i=1;i<=9;i++){
+        solve(i,n);
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    lexicographicalNumber(n);
+}
