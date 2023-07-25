a9c407526c658c44db6dd8bd0e1b628cb879db76
Vivek kumar <vivekkumar52522@gmail.com>
2023-07-25 18:54:34 +0530

    Day_69

---
+++ Day_69/PeekELementInMountainArr.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+       int low=0;
+        int high=arr.size()-1;
+        int mid;
+        while(low<high){
+            mid=low+(high-low)/2;
+           if(arr[mid]<arr[mid+1]){
+               low=mid+1;
+           }else{
+               high=mid;
+           }
+        }
+        return low;
+    }
+};
